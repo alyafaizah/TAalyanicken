b35f78a5d0690f4c82cc91a58c9a67d033cd748c
alyafaizah <alya.faizah01@gmail.com>
2022-03-12 12:52:58 +0700

    create ticket page

--- resources/views/modules/tiket/createtiket.blade.php
+++ resources/views/modules/tiket/createtiket.blade.php
@@ -13,74 +13,31 @@
                 <!--begin::Card-->
                 <div class="card card-custom gutter-b example example-compact">
                     <div class="card-header">
-                        <h3 class="card-title">Base Controls</h3>
-                        <div class="card-toolbar">
-                            <div class="example-tools justify-content-center">
-                                <span class="example-toggle" data-toggle="tooltip" title="View code"></span>
-                                <span class="example-copy" data-toggle="tooltip" title="Copy code"></span>
-                            </div>
-                        </div>
+                        <h3 class="card-title">Tambah Tiket</h3>
                     </div>
                     <!--begin::Form-->
                     <form>
                         <div class="card-body">
-                            <div class="form-group mb-8">
-                                <div class="alert alert-custom alert-default" role="alert">
-                                    <div class="alert-icon">
-                                        <span class="svg-icon svg-icon-primary svg-icon-xl">
-                                            <!--begin::Svg Icon | path:/metronic/theme/html/demo4/dist/assets/media/svg/icons/Tools/Compass.svg-->
-                                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-                                                <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                                    <rect x="0" y="0" width="24" height="24" />
-                                                    <path d="M7.07744993,12.3040451 C7.72444571,13.0716094 8.54044565,13.6920474 9.46808594,14.1079953 L5,23 L4.5,18 L7.07744993,12.3040451 Z M14.5865511,14.2597864 C15.5319561,13.9019016 16.375416,13.3366121 17.0614026,12.6194459 L19.5,18 L19,23 L14.5865511,14.2597864 Z M12,3.55271368e-14 C12.8284271,3.53749572e-14 13.5,0.671572875 13.5,1.5 L13.5,4 L10.5,4 L10.5,1.5 C10.5,0.671572875 11.1715729,3.56793164e-14 12,3.55271368e-14 Z" fill="#000000" opacity="0.3" />
-                                                    <path d="M12,10 C13.1045695,10 14,9.1045695 14,8 C14,6.8954305 13.1045695,6 12,6 C10.8954305,6 10,6.8954305 10,8 C10,9.1045695 10.8954305,10 12,10 Z M12,13 C9.23857625,13 7,10.7614237 7,8 C7,5.23857625 9.23857625,3 12,3 C14.7614237,3 17,5.23857625 17,8 C17,10.7614237 14.7614237,13 12,13 Z" fill="#000000" fill-rule="nonzero" />
-                                                </g>
-                                            </svg>
-                                            <!--end::Svg Icon-->
-                                        </span>
-                                    </div>
-                                    <div class="alert-text">The example form below demonstrates common HTML form elements that receive updated styles from Bootstrap with additional classes.</div>
-                                </div>
-                            </div>
                             <div class="form-group">
-                                <label>Email address
-                                    <span class="text-danger">*</span></label>
-                                <input type="email" class="form-control" placeholder="Enter email" />
-                                <span class="form-text text-muted">We'll never share your email with anyone else.</span>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Password
-                                    <span class="text-danger">*</span></label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                            </div>
-                            <div class="form-group">
-                                <label>Static:</label>
-                                <p class="form-control-plaintext text-muted">email@example.com</p>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleSelect1">Example select
+                                <label for="exampleSelect1">Jenis
                                     <span class="text-danger">*</span></label>
                                 <select class="form-control" id="exampleSelect1">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
+                                    <option>Weekdays</option>
+                                    <option>Weekend</option>
                                 </select>
                             </div>
                             <div class="form-group">
-                                <label for="exampleSelect2">Example multiple select
+                                <label>Harga
                                     <span class="text-danger">*</span></label>
-                                <select multiple="multiple" class="form-control" id="exampleSelect2">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                </select>
+                                <input type="email" class="form-control" placeholder="" />
+                            </div>
+                            <div class="form-group">
+                                <label>Stok
+                                    <span class="text-danger">*</span></label>
+                                <input type="email" class="form-control" placeholder="" />
                             </div>
                             <div class="form-group mb-1">
-                                <label for="exampleTextarea">Example textarea
+                                <label for="exampleTextarea">Keterangan
                                     <span class="text-danger">*</span></label>
                                 <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
                             </div>
--- resources/views/modules/tiket/datatiket.blade.php
+++ resources/views/modules/tiket/datatiket.blade.php
@@ -20,10 +20,6 @@
 				<div class="modal-body">
 					<div id="kt_subheader_leaflet" style="height:450px; width: 100%;"></div>
 				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-light-primary font-weight-bold" data-dismiss="modal">Cancel</button>
-					<button id="submit" type="button" class="btn btn-primary font-weight-bold" data-dismiss="modal">Apply</button>
-				</div>
 			</div>
 		</div>
 	</div>
@@ -36,77 +32,11 @@
 			<div class="card card-custom">
 				<div class="card-header flex-wrap border-0 pt-6 pb-0">
 					<div class="card-title">
-						<h3 class="card-label">Local Datasource
-							<span class="text-muted pt-2 font-size-sm d-block">Javascript array as data source</span></h3>
+						<h1 class="card-label">Data Tiket</h1>
 					</div>
 					<div class="card-toolbar">
-						<!--begin::Dropdown-->
-						<div class="dropdown dropdown-inline mr-2">
-							<button type="button" class="btn btn-light-primary font-weight-bolder dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<span class="svg-icon svg-icon-md">
-									<!--begin::Svg Icon | path:/metronic/theme/html/demo4/dist/assets/media/svg/icons/Design/PenAndRuller.svg-->
-									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
-										<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-											<rect x="0" y="0" width="24" height="24" />
-											<path d="M3,16 L5,16 C5.55228475,16 6,15.5522847 6,15 C6,14.4477153 5.55228475,14 5,14 L3,14 L3,12 L5,12 C5.55228475,12 6,11.5522847 6,11 C6,10.4477153 5.55228475,10 5,10 L3,10 L3,8 L5,8 C5.55228475,8 6,7.55228475 6,7 C6,6.44771525 5.55228475,6 5,6 L3,6 L3,4 C3,3.44771525 3.44771525,3 4,3 L10,3 C10.5522847,3 11,3.44771525 11,4 L11,19 C11,19.5522847 10.5522847,20 10,20 L4,20 C3.44771525,20 3,19.5522847 3,19 L3,16 Z" fill="#000000" opacity="0.3" />
-											<path d="M16,3 L19,3 C20.1045695,3 21,3.8954305 21,5 L21,15.2485298 C21,15.7329761 20.8241635,16.200956 20.5051534,16.565539 L17.8762883,19.5699562 C17.6944473,19.7777745 17.378566,19.7988332 17.1707477,19.6169922 C17.1540423,19.602375 17.1383289,19.5866616 17.1237117,19.5699562 L14.4948466,16.565539 C14.1758365,16.200956 14,15.7329761 14,15.2485298 L14,5 C14,3.8954305 14.8954305,3 16,3 Z" fill="#000000" />
-										</g>
-									</svg>
-									<!--end::Svg Icon-->
-								</span>Export</button>
-							<!--begin::Dropdown Menu-->
-							<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
-								<!--begin::Navigation-->
-								<ul class="navi flex-column navi-hover py-2">
-									<li class="navi-header font-weight-bolder text-uppercase font-size-sm text-primary pb-2">Choose an option:</li>
-									<li class="navi-item">
-										<a href="#" class="navi-link">
-											<span class="navi-icon">
-												<i class="la la-print"></i>
-											</span>
-											<span class="navi-text">Print</span>
-										</a>
-									</li>
-									<li class="navi-item">
-										<a href="#" class="navi-link">
-											<span class="navi-icon">
-												<i class="la la-copy"></i>
-											</span>
-											<span class="navi-text">Copy</span>
-										</a>
-									</li>
-									<li class="navi-item">
-										<a href="#" class="navi-link">
-											<span class="navi-icon">
-												<i class="la la-file-excel-o"></i>
-											</span>
-											<span class="navi-text">Excel</span>
-										</a>
-									</li>
-									<li class="navi-item">
-										<a href="#" class="navi-link">
-											<span class="navi-icon">
-												<i class="la la-file-text-o"></i>
-											</span>
-											<span class="navi-text">CSV</span>
-										</a>
-									</li>
-									<li class="navi-item">
-										<a href="#" class="navi-link">
-											<span class="navi-icon">
-												<i class="la la-file-pdf-o"></i>
-											</span>
-											<span class="navi-text">PDF</span>
-										</a>
-									</li>
-								</ul>
-								<!--end::Navigation-->
-							</div>
-							<!--end::Dropdown Menu-->
-						</div>
-						<!--end::Dropdown-->
 						<!--begin::Button-->
-						<a href="#" class="btn btn-primary font-weight-bolder">
+						<a href="/createtiket" class="btn btn-primary font-weight-bolder">
 							<span class="svg-icon svg-icon-md">
 								<!--begin::Svg Icon | path:/metronic/theme/html/demo4/dist/assets/media/svg/icons/Design/Flatten.svg-->
 								<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
@@ -117,7 +47,7 @@
 									</g>
 								</svg>
 								<!--end::Svg Icon-->
-							</span>New Record</a>
+							</span>Tambah Tiket</a>
 						<!--end::Button-->
 					</div>
 				</div>
@@ -171,7 +101,127 @@
 					<!--end::Search Form-->
 					<!--end: Search Form-->
 					<!--begin: Datatable-->
-					<div class="datatable datatable-bordered datatable-head-custom" id="kt_datatable"></div>
+					<div class="datatable datatable-bordered datatable-head-custom datatable-default datatable-primary datatable-loaded" id="kt_datatable" style="">
+						<table class="datatable-table" style="display: block;">
+							<thead class="datatable-head">
+								<tr class="datatable-row" style="left: 0px;">
+									<th data-field="RecordID" class="datatable-cell-center datatable-cell datatable-cell-check">
+										<span style="width: 20px;">
+											<label class="checkbox checkbox-single checkbox-all">
+												<input type="checkbox">&nbsp;
+												<span>
+												</span>
+											</label>
+										</span>
+									</th>
+									<th data-field="OrderID" class="datatable-cell datatable-cell-sort">
+										<span style="width: 108px;">No</span>
+									</th>
+									<th data-field="Country" class="datatable-cell datatable-cell-sort">
+										<span style="width: 108px;">Jenis Tiket</span>
+									</th>
+									<th data-field="ShipDate" class="datatable-cell datatable-cell-sort">
+										<span style="width: 108px;">Harga</span>
+									</th>
+									<th data-field="CompanyName" class="datatable-cell datatable-cell-sort">
+										<span style="width: 108px;">Stok</span>
+									</th>
+									<<th data-field="ShipDate" class="datatable-cell datatable-cell-sort">
+										<span style="width: 108px;">Keterangan</span>
+										</th>
+										<th data-field="ShipDate" class="datatable-cell datatable-cell-sort">
+											<span style="width: 108px;">Record</span>
+										</th>
+										<th data-field="Actions" data-autohide-disabled="false" class="datatable-cell datatable-cell-sort">
+											<span style="width: 125px;">Actions</span>
+										</th>
+								</tr>
+							</thead>
+
+							<tbody class="datatable-body" style="">
+								<tr data-row="0" class="datatable-row" style="left: 0px;">
+									<td class="datatable-cell-center datatable-cell datatable-cell-check" data-field="RecordID" aria-label="81"><span style="width: 20px;"><label class="checkbox checkbox-single"><input type="checkbox" value="81">&nbsp;<span></span></label></span></td>
+									<td data-field="OrderID" aria-label="68084-198" class="datatable-cell"><span style="width: 108px;">68084-198</span></td>
+									<td data-field="Country" aria-label="Philippines" class="datatable-cell"><span style="width: 108px;">Philippines PH</span></td>
+									<td data-field="ShipDate" aria-label="10/4/2017" class="datatable-cell"><span style="width: 108px;">10/4/2017</span></td>
+									<td class="datatable-cell-sorted datatable-cell" data-field="CompanyName" aria-label="Adams, Macejkovic and Little"><span style="width: 108px;">Adams, Macejkovic and Little</span></td>
+									<td data-field="Status" aria-label="2" class="datatable-cell"><span style="width: 108px;"><span class="label font-weight-bold label-lg  label-light-danger label-inline">Delivered</span></span></td>
+									<td data-field="Type" data-autohide-disabled="false" aria-label="3" class="datatable-cell"><span style="width: 108px;"><span class="label label-success label-dot mr-2"></span><span class="font-weight-bold text-success">Direct</span></span></td>
+									<td data-field="Actions" data-autohide-disabled="false" aria-label="null" class="datatable-cell"><span style="overflow: visible; position: relative; width: 125px;">
+											<div class="dropdown dropdown-inline"> <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mr-2" data-toggle="dropdown"> <span class="svg-icon svg-icon-md"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+															<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+																<rect x="0" y="0" width="24" height="24"></rect>
+																<path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#000000"></path>
+															</g>
+														</svg> </span> </a>
+												<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+													<ul class="navi flex-column navi-hover py-2">
+														<li class="navi-header font-weight-bolder text-uppercase font-size-xs text-primary pb-2"> Choose an action: </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-print"></i></span> <span class="navi-text">Print</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-copy"></i></span> <span class="navi-text">Copy</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-file-excel-o"></i></span> <span class="navi-text">Excel</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-file-text-o"></i></span> <span class="navi-text">CSV</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-file-pdf-o"></i></span> <span class="navi-text">PDF</span> </a> </li>
+													</ul>
+												</div>
+											</div> <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mr-2" title="Edit details"> <span class="svg-icon svg-icon-md"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+														<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+															<rect x="0" y="0" width="24" height="24"></rect>
+															<path d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z" fill="#000000" fill-rule="nonzero" transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) "></path>
+															<rect fill="#000000" opacity="0.3" x="5" y="20" width="15" height="2" rx="1"></rect>
+														</g>
+													</svg> </span> </a> <a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete"> <span class="svg-icon svg-icon-md"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+														<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+															<rect x="0" y="0" width="24" height="24"></rect>
+															<path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fill-rule="nonzero"></path>
+															<path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3"></path>
+														</g>
+													</svg> </span> </a>
+										</span></td>
+								</tr>
+								<tr data-row="1" class="datatable-row datatable-row-even" style="left: 0px;">
+									<td class="datatable-cell-center datatable-cell datatable-cell-check" data-field="RecordID" aria-label="50"><span style="width: 20px;"><label class="checkbox checkbox-single"><input type="checkbox" value="50">&nbsp;<span></span></label></span></td>
+									<td data-field="OrderID" aria-label="54473-254" class="datatable-cell"><span style="width: 108px;">54473-254</span></td>
+									<td data-field="Country" aria-label="Australia" class="datatable-cell"><span style="width: 108px;">Australia AU</span></td>
+									<td data-field="ShipDate" aria-label="9/6/2016" class="datatable-cell"><span style="width: 108px;">9/6/2016</span></td>
+									<td class="datatable-cell-sorted datatable-cell" data-field="CompanyName" aria-label="Ankunding-Hudson"><span style="width: 108px;">Ankunding-Hudson</span></td>
+									<td data-field="Status" aria-label="2" class="datatable-cell"><span style="width: 108px;"><span class="label font-weight-bold label-lg  label-light-danger label-inline">Delivered</span></span></td>
+									<td data-field="Type" data-autohide-disabled="false" aria-label="1" class="datatable-cell"><span style="width: 108px;"><span class="label label-danger label-dot mr-2"></span><span class="font-weight-bold text-danger">Online</span></span></td>
+									<td data-field="Actions" data-autohide-disabled="false" aria-label="null" class="datatable-cell"><span style="overflow: visible; position: relative; width: 125px;">
+											<div class="dropdown dropdown-inline"> <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mr-2" data-toggle="dropdown"> <span class="svg-icon svg-icon-md"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+															<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+																<rect x="0" y="0" width="24" height="24"></rect>
+																<path d="M5,8.6862915 L5,5 L8.6862915,5 L11.5857864,2.10050506 L14.4852814,5 L19,5 L19,9.51471863 L21.4852814,12 L19,14.4852814 L19,19 L14.4852814,19 L11.5857864,21.8994949 L8.6862915,19 L5,19 L5,15.3137085 L1.6862915,12 L5,8.6862915 Z M12,15 C13.6568542,15 15,13.6568542 15,12 C15,10.3431458 13.6568542,9 12,9 C10.3431458,9 9,10.3431458 9,12 C9,13.6568542 10.3431458,15 12,15 Z" fill="#000000"></path>
+															</g>
+														</svg> </span> </a>
+												<div class="dropdown-menu dropdown-menu-sm dropdown-menu-right">
+													<ul class="navi flex-column navi-hover py-2">
+														<li class="navi-header font-weight-bolder text-uppercase font-size-xs text-primary pb-2"> Choose an action: </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-print"></i></span> <span class="navi-text">Print</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-copy"></i></span> <span class="navi-text">Copy</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-file-excel-o"></i></span> <span class="navi-text">Excel</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-file-text-o"></i></span> <span class="navi-text">CSV</span> </a> </li>
+														<li class="navi-item"> <a href="#" class="navi-link"> <span class="navi-icon"><i class="la la-file-pdf-o"></i></span> <span class="navi-text">PDF</span> </a> </li>
+													</ul>
+												</div>
+											</div> <a href="javascript:;" class="btn btn-sm btn-clean btn-icon mr-2" title="Edit details"> <span class="svg-icon svg-icon-md"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+														<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+															<rect x="0" y="0" width="24" height="24"></rect>
+															<path d="M8,17.9148182 L8,5.96685884 C8,5.56391781 8.16211443,5.17792052 8.44982609,4.89581508 L10.965708,2.42895648 C11.5426798,1.86322723 12.4640974,1.85620921 13.0496196,2.41308426 L15.5337377,4.77566479 C15.8314604,5.0588212 16,5.45170806 16,5.86258077 L16,17.9148182 C16,18.7432453 15.3284271,19.4148182 14.5,19.4148182 L9.5,19.4148182 C8.67157288,19.4148182 8,18.7432453 8,17.9148182 Z" fill="#000000" fill-rule="nonzero" transform="translate(12.000000, 10.707409) rotate(-135.000000) translate(-12.000000, -10.707409) "></path>
+															<rect fill="#000000" opacity="0.3" x="5" y="20" width="15" height="2" rx="1"></rect>
+														</g>
+													</svg> </span> </a> <a href="javascript:;" class="btn btn-sm btn-clean btn-icon" title="Delete"> <span class="svg-icon svg-icon-md"> <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+														<g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+															<rect x="0" y="0" width="24" height="24"></rect>
+															<path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fill-rule="nonzero"></path>
+															<path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3"></path>
+														</g>
+													</svg> </span> </a>
+										</span></td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
 					<!--end: Datatable-->
 				</div>
 			</div>
